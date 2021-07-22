01cc8fc4bc49cf8594052284bb4068c83118e7d0
Julio Alejandro Santos Corona <59350645+jualesac@users.noreply.github.com>
2021-07-22 00:11:42 -0500

    Reestructura de clases

--- ext/scheme/scheme.php
+++ ext/scheme/scheme.php
@@ -16,11 +16,45 @@ use Exception;
 
 final class SCHEME extends SCHFile
 {
+    private bool $__isObject;
+
     public function __construct (array $schema) {
         parent::__construct ($schema);
+
+        $this->__isObject = false;
     }
 
-    final public function testLine () : array|null {
+    final public function test (&$values) : bool {
+        $this->__isObject = is_object ($values);
+        $result = true;
+
+        foreach ($this->__schema as $name => $properties) {
+            (bool) $require = (substr($name, -1) != "*") ? true : false;
+            (string) $name = str_replace ("*", "", $name);
+            
+            if ( !$require && ($this->__isObject ? !isset($values->$name) : !isset($values[$name])) ) {
+                continue;
+            }
+
+            if ( $require && ($this->__isObject ? !isset($values->$name) : !isset($values[$name])) ) {
+                return $this->boolStrict (false, "Schema structure not localized: {$name}");
+            }
+
+            $prop = $this->propertiesConstructor (is_array($properties) ? $properties : [ $properties ]);
+
+            if ($this->__isObject) {
+                $result = $this->checkType ($values->$name, $prop[0], $prop[1], $prop[2]);
+            } else {
+                $result = $this->checkType ($values[$name], $prop[0], $prop[1], $prop[2]);
+            }
+
+            if (!$result) { return false; }
+        }
+
+        return true;
+    }
+
+    final public function testLine () {
         $line = $this->getLine ();
         $newLine = [];
 
@@ -58,5 +92,33 @@ final class SCHEME extends SCHFile
         }
 
         return $newLine;
+    }
+
+    private function propertiesConstructor (array $args) : array {
+        if (!isset($args[1])) {
+            return [
+                $args[0],
+                true,
+                function ($v) { return $v; }
+            ];
+        }
+
+        if (is_bool($args[1])) {
+            return [
+                $args[0],
+                $args[1],
+                is_callable ($args[2] ?? 1) ? $args[2] : function ($v) { return $v; }
+            ];
+        }
+
+        if (is_callable($args[1])) {
+            return [
+                $args[0],
+                true,
+                $args[1]
+            ];
+        }
+
+        throw new Exception ("Incorrect schema structure");
     }
 }
