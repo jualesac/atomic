a3021ec26bc03acbee5cc5ab6145afe518b4c83a
Julio Alejandro Santos Corona <59350645+jualesac@users.noreply.github.com>
2021-08-04 01:33:58 -0500

    Versión 2.0

--- http/resolve/response.php
+++ http/resolve/response.php
@@ -54,7 +54,7 @@ final class RESPONSE
         $this->__utf8 = $c;
     }
 
-    private function arrayEncode ($content) : array {
+    private function arrayEncode (array|object $content) : array {
         if (is_array($content)) { return $content; }
         if (is_object($content)) { return (array) $content; }
 
