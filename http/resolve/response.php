7d32836fa1f8898044877e4cccab8b5bfe5bf55b
Julio Alejandro Santos Corona <59350645+jualesac@users.noreply.github.com>
2021-08-05 00:35:19 -0500

    Versión 2.0

--- http/resolve/response.php
+++ http/resolve/response.php
@@ -10,54 +10,44 @@
 
 namespace http\resolve;
 
-final class RESPONSE
+trait RESCONSTRUCT
 {
-    public int $__state;
-    public array $__content;
-    public bool $__utf8;
-    public bool $__utf8isNull = true;
+    private array $__res;
 
-    final public function __construct ($args) {
+    private function responseConstruct ($args) : void {
         if (is_array($args[1] ?? null)) {
-
-            $this->__utf8isNull = isset($args[2]) ? false : true;
-            $this->__construct0 ($args[0], $args[1], ( $args[2] ?? false ));
-
+            $this->__resConstruct0 ($args[0], $args[1], ($args[2] ?? null));
         } elseif (is_array($args[0])) {
-
-            $this->__utf8isNull = isset($args[1]) ? false : true;
-            $this->__construct1 ($args[0], ( $args[1] ?? false ));
-
-        } elseif (is_int((int) $args[0]) && !is_null($args[1])) {
-            
-            $this->__utf8isNull = isset($args[2]) ? false : true;
-            $this->__construct2 ($args[0], $args[1], ( $args[2] ?? false ));
-            
+            $this->__resConstruct1 ($args[0], ($args[1] ?? null));
+        }elseif (is_int((int) $args[0]) && !is_null($args[1])) {
+            $this->__resConstruct2 ($args[0], $args[1], ($args[2] ?? null));
         }
     }
 
-    private function __construct0 (int $a, array $b, bool $c = false) : void {
-        $this->__state = $a;
-        $this->__content = $b;
-        $this->__utf8 = $c;
+    private function __resConstruct0 (int $a, array $b, bool $c = null) : void {
+        $this->__res = [ $a, $b, $c ];
     }
 
-    private function __construct1 (array $a, bool $b = false) : void {
-        $this->__state = (int) ($a["state"] ?? $a[0]);
-        $this->__content = $this->arrayEncode ( $a["content"] ?? ($a["message"] ?? $a[1]) );
-        $this->__utf8 = $b;
+    private function __resConstruct1 (array $a, bool $b = null) : void {
+        $this->__res = [
+            (int) ($a["state"] ?? $a[0]),
+            $this->__arrayEncode ($a["content"] ?? ($a["message"] ?? $a[1])),
+            $b
+        ];
     }
 
-    private function __construct2 (int $a, $b, bool $c = false) : void {
-        $this->__state = $a;
-        $this->__content = $this->arrayEncode ($b);
-        $this->__utf8 = $c;
+    private function __resConstruct2 (int $a, $b, bool $c = null) : void {
+        $this->__res = [
+            (int) $a,
+            $this->__arrayEncode ($b),
+            $c
+        ];
     }
 
-    private function arrayEncode (array|object $content) : array {
-        if (is_array($content)) { return $content; }
-        if (is_object($content)) { return (array) $content; }
+    private function __arrayEncode ($c) : array {
+        if (is_array($c)) { return $c; }
+        if (is_object($c)) { return (array) $c; }
 
-        return [ "message" => $content ];
+        return [ "message" => $c ];
     }
 }
