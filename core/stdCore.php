1df437cdca730ef49c5ea77a02b7c01cacc79f70
Julio Alejandro Santos Corona <59350645+jualesac@users.noreply.github.com>
2021-12-04 10:54:16 -0600

    Versión 2.0

--- core/stdCore.php
+++ core/stdCore.php
@@ -26,28 +26,21 @@ abstract class STDCore extends DBCORE
 {
     use FUNCTIONCORE;
 
-    private MIDDLEWARE $middle;
     protected HTTPRoute $route;
 
     protected function __construct () {
         parent::__construct ();
 
         $this->route = new HTTPRoute;
-        $this->middle = new MIDDLEWARE;
+        $middle = new MIDDLEWARE;
 
         $this->setRoutes ();
-        $this->middlewareToRoutes ($this->middle->get());
+        $middle->setMiddles ($this->route);
     }
 
     abstract protected function setRoutes () : void;
 
     final public function getRoutes () : HTTPRoute {
         return $this->route;
-    }
-
-    private function middlewareToRoutes (SplFixedArray $middle) : void {
-        foreach ($middle as $m) {
-            $this->route->setMiddleware ($m[0], $m[1]);
-        }
     }
 }
